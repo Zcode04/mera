9bf6e83e73ed21ab7a7e29ce653033ae72643a87
zein <Zcode0430@gmail.com>
2025-05-05 08:51:32 -1200

    unecha

--- src/app/page.js
+++ src/app/page.js
@@ -107,8 +107,7 @@ export default function Home() {
       Profile Tab
     </h3>
     <p className="mb-2">
-      This is some placeholder content the Profile tab's associated content,
-      clicking another tab will toggle the visibility of this one for the next.
+      
     </p>
     <p>
       The tab JavaScript swaps classes to control the content visibility and
